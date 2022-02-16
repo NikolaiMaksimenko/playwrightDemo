0e9e3d74d89d4ae89c61e649cb9490355c8d9747
nmaksimenko <nmaksimenko@questrade.com>
2022-02-16 19:43:47 +0200

    homework with annotations

--- cool-project/anna-hw1.spec.mjs
+++ cool-project/anna-hw1.spec.mjs
@@ -3,23 +3,21 @@ import { test, expect } from '@playwright/test';
 const searchField = '[class="YacQv gsfi"]';
 
 test('first test', async ({ page }) => {
-  await page.goto('https://www.google.com/', {waitUntil: 'load'});
-  const googleSearch = await page.waitForSelector(searchField, {
-    state: 'visible',
-    timeout: 5000,
-});
-  await googleSearch.type('Playwright', { delay: 100 });
-  await Promise.all([
-    page.waitForNavigation({ waitUntil: 'networkidle' }),
-    page.keyboard.press('Enter'),
+    await page.goto('https://www.google.com/');
+    const googleSearch = await page.waitForSelector(searchField, {
+        state: 'visible',
+        timeout: 5000,
+    });
+    await googleSearch.type('Playwright', { delay: 50 });
+    await Promise.all([
+        page.waitForNavigation({ waitUntil: 'networkidle' }),
+        page.keyboard.press('Enter'),
     ]);
-  await page.click('text=Playwright: Fast and reliable end-to-end testing for modern ...');
-  await expect(page).toHaveURL('https://playwright.dev/');
-  const getStarted = await page.waitForSelector('[class="getStarted_1iQB"]',{
-    state: 'visible',
-    timeout: 5000,
-  });
-  await getStarted.click();
-  await expect(page).toHaveURL('https://playwright.dev/docs/intro');
-  await page.close();
+    await page.click('text=Playwright: Fast and reliable end-to-end testing for modern ...');
+    const getStarted = await page.waitForSelector('[class="getStarted_1iQB"]', {
+        state: 'visible',
+        timeout: 5000,
+    });
+    await getStarted.click();
+    expect(page).toHaveURL('https://playwright.dev/docs/intro');
 });
