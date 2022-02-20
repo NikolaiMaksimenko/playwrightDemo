0d06ab71cfb569269447f3c9649792a05264b7c0
Arthur <aradomskyi@n-ix.com>
2022-02-20 19:29:56 +0200

    Merge branch 'Arthur' of https://github.com/NikolaiMaksimenko/playwrightDemo into Arthur

---
+++ cool-project/arthur-hw2.spec.mjs
@@ -0,0 +1,36 @@
+import { test } from '@playwright/test';
+
+const googleLocator = '[class="gLFyf gsfi"]';
+
+test.describe('Tests of the search feature', () => {
+
+    test('input "Google" text and search it', async ({ page }) => {
+        await page.goto('https://www.google.com/', { waitUntil: 'networkidle' });
+        const googleSearch = await page.waitForSelector(googleLocator, {
+            state: 'visible',
+            timeout: 5000,
+        });
+        await googleSearch.type('Google');
+        await Promise.all([
+            page.waitForNavigation({ waitUntil: 'networkidle' }),
+            page.keyboard.press('Enter'),
+        ]);
+        await page.screenshot({ path: 'screenshots/Test1' + Date.now() + '.png', fullPage: true });
+        await page.close();
+    });
+
+    test('Test with empty input field', async ({ page }) => {
+        await page.goto('https://www.google.com/', { waitUntil: 'networkidle' });
+        const googleSearch = await page.waitForSelector(googleLocator, {
+            state: 'visible',
+            timeout: 5000,
+        });
+        await googleSearch.type('Playwright');
+        await Promise.all([
+            page.waitForNavigation({ waitUntil: 'networkidle' }),
+            page.keyboard.press('Enter'),
+        ]);
+        await page.screenshot({ path: 'screenshots/Test1' + Date.now() + '.png', fullPage: true });
+        await page.close();
+    });
+});
