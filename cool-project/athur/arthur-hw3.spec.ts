210324007dd2719cd54f14d25702174a5188abd8
nmaksimenko <nmaksimenko@questrade.com>
2022-04-20 18:17:49 +0300

    reviewed homework

--- cool-project/athur/arthur-hw3.spec.ts
+++ cool-project/athur/arthur-hw3.spec.ts
@@ -1,21 +1,25 @@
 import { expect, test } from '@playwright/test';
-import { categoryMenu, subCategoryMenu, carouselCategory, aSideCategoryMenu, priceText, prices, } from '../athur/page-object/locators';
+import {
+    categoryMenu,
+    subCategoryMenu,
+    carouselCategory,
+    priceText,
+    prices,
+} from '../athur/page-object/locators';
 
 test.describe('Comp&Parts testing', () => {
     test('Get AM4 CPU', async ({ page }) => {
         await page.goto('https://ebay.com', { waitUntil: 'networkidle' });
         await page.locator(categoryMenu, { hasText: 'Shop by category' }).click();
         await page.locator(subCategoryMenu, { hasText: 'Computers & Tablets' }).click();
-        await page
-            .locator(carouselCategory, { hasText: 'Computer Components & Parts' })
-            .click();
-        await page.locator(carouselCategory, { hasText: 'CPUs/Processors'}).click();
+        await page.locator(carouselCategory, { hasText: 'Computer Components & Parts' }).click();
+        await page.locator(carouselCategory, { hasText: 'CPUs/Processors' }).click();
 
         await page.locator(carouselCategory, { hasText: 'Socket AM4' }).click();
         await page.waitForSelector(prices);
         const getPrice = await page.locator(priceText).allInnerTexts();
 
-        expect(console.log(getPrice[0]));
-  
-    })
-})
+        // fix expect to check real value
+        expect(getPrice[0]).toBe('453$');
+    });
+});
